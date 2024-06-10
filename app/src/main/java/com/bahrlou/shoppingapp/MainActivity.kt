6075ecc685638b303fc0f32ca1aa91696743f665
mona-baharlou <mona.baharlou755@gmail.com>
2024-06-10 14:56:30 +0330

    Navigation and related screens added to MainActivity.kt

--- app/src/main/java/com/bahrlou/shoppingapp/MainActivity.kt
+++ app/src/main/java/com/bahrlou/shoppingapp/MainActivity.kt
@@ -11,6 +11,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.bahrlou.shoppingapp.ui.theme.ShoppingAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,6 +26,103 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ShoppingUi() {
+
+
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = "mainScreen"
+    ) {
+
+        composable("mainScreen") {
+            MainScreen()
+        }
+
+
+        composable("productScreen") {
+            ProductScreen()
+        }
+
+        composable("categoryScreen") {
+            CategoryScreen()
+        }
+
+        composable("profileScreen") {
+            ProfileScreen()
+        }
+
+        composable("cartScreen") {
+            CartScreen()
+        }
+
+        composable("signUpScreen") {
+            SignUpScreen()
+        }
+
+        composable("signInScreen") {
+            SignInScreen()
+        }
+
+        composable("introScreen") {
+            IntroScreen()
+        }
+
+        composable("noInternetScreen") {
+            NoInternetScreen()
+        }
+
+    }
+}
+
+@Composable
+fun NoInternetScreen() {
+
+    
+}
+
+@Composable
+fun IntroScreen() {
+
+
+}
+
+@Composable
+fun SignInScreen() {
+
+
+}
+
+@Composable
+fun SignUpScreen() {
+
+
+}
+
+@Composable
+fun CartScreen() {
+
+
+}
+
+@Composable
+fun ProfileScreen() {
+
+
+}
+
+@Composable
+fun CategoryScreen() {
+
+}
+
+@Composable
+fun ProductScreen() {
+
+
+}
+
+@Composable
+fun MainScreen() {
 
 }
 
