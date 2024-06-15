c50512e68d83cb54d9c79aad2e23f578f026e1d8
mona-baharlou <mona.baharlou755@gmail.com>
2024-06-15 15:26:03 +0330

    MainScreen.kt updated--> CategorySection & CategoryItem code added

--- app/src/main/java/com/bahrlou/shoppingapp/ui/features/main/MainScreen.kt
+++ app/src/main/java/com/bahrlou/shoppingapp/ui/features/main/MainScreen.kt
@@ -1,8 +1,12 @@
 package com.bahrlou.shoppingapp.ui.features.main
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Icon
@@ -15,11 +19,17 @@ import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.bahrlou.shoppingapp.R
 import com.bahrlou.shoppingapp.ui.theme.BackgroundMain
+import com.bahrlou.shoppingapp.ui.theme.CardBackground
+import com.bahrlou.shoppingapp.ui.theme.Shapes
 import com.bahrlou.shoppingapp.ui.theme.ShoppingAppTheme
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
@@ -29,8 +39,7 @@ import com.google.accompanist.systemuicontroller.rememberSystemUiController
 fun MainScreenPreview() {
     ShoppingAppTheme {
         Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = BackgroundMain
+            modifier = Modifier.fillMaxSize(), color = BackgroundMain
         ) {
             MainScreen()
         }
@@ -64,8 +73,7 @@ fun MainScreen() {
 @Composable
 fun TopToolbar() {
 
-    TopAppBar(
-        backgroundColor = Color.White,
+    TopAppBar(backgroundColor = Color.White,
         title = { Text(text = "Shopping App") },
         elevation = 0.dp,
         actions = {
@@ -76,8 +84,7 @@ fun TopToolbar() {
             IconButton(onClick = {}) {
                 Icon(Icons.Default.Person, null)
             }
-        }
-    )
+        })
 
 }
 
@@ -85,7 +92,42 @@ fun TopToolbar() {
 
 @Composable
 fun CategorySection() {
+    LazyRow(
+        modifier = Modifier.padding(top = 16.dp), contentPadding = PaddingValues(end = 16.dp)
+    ) {
 
+        items(10) {
+            CategoryItem()
+        }
+    }
+}
+
+@Composable
+fun CategoryItem() {
+    Column(
+        modifier = Modifier
+            .padding(start = 16.dp)
+            .clickable {},
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Surface(
+            shape = Shapes.medium,
+            color = CardBackground
+        ) {
+            Image(
+                modifier = Modifier.padding(16.dp),
+                painter = painterResource(id = R.drawable.ic_icon_app),
+                contentDescription = null
+            )
+        }
+
+
+        Text(
+            text = "Bags", modifier = Modifier
+                .padding(top = 4.dp),
+            style = TextStyle(color = Color.Gray)
+        )
+    }
 }
 
 //**************************** Products **********************************/
--- app/src/main/java/com/bahrlou/shoppingapp/ui/theme/Color.kt
+++ app/src/main/java/com/bahrlou/shoppingapp/ui/theme/Color.kt
@@ -4,7 +4,7 @@ import androidx.compose.ui.graphics.Color
 
 val BackgroundMain = Color(255, 255, 255, 255)
 val SearchItemsBackground = Color(251, 250, 255)
-val CardBackground = Color(218, 218, 218)
+val CardBackground = Color(9, 121, 223,8)
 val Blue = Color(9, 121, 223)
 
 //text->white, black, darkGray
