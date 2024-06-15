4bd6baba237322ef11997444f16f48c2a3374b66
mona-baharlou <mona.baharlou755@gmail.com>
2024-06-16 00:05:17 +0330

    Modules.kt updated -> implemented RoomDb and MainViewModel

--- app/src/main/java/com/bahrlou/shoppingapp/di/Modules.kt
+++ app/src/main/java/com/bahrlou/shoppingapp/di/Modules.kt
@@ -1,9 +1,14 @@
 package com.bahrlou.shoppingapp.di
 
 import android.content.Context
+import androidx.room.Room
+import com.bahrlou.shoppingapp.model.db.AppDatabase
 import com.bahrlou.shoppingapp.model.net.createApiService
+import com.bahrlou.shoppingapp.model.repository.product.ProductRepository
+import com.bahrlou.shoppingapp.model.repository.product.ProductRepositoryImpl
 import com.bahrlou.shoppingapp.model.repository.user.UserRepository
 import com.bahrlou.shoppingapp.model.repository.user.UserRepositoryImpl
+import com.bahrlou.shoppingapp.ui.features.main.MainViewModel
 import com.bahrlou.shoppingapp.ui.features.signIn.SignInViewModel
 import com.bahrlou.shoppingapp.ui.features.signUp.SignUpViewModel
 import org.koin.android.ext.koin.androidContext
@@ -16,9 +21,26 @@ val shoppingModules = module {
 
     single { createApiService() }
 
+    single {
+        Room.databaseBuilder(androidContext(), AppDatabase::class.java, "shopping_database.db")
+            .build()
+    }
+
+
+    //Repositories
     single<UserRepository> { UserRepositoryImpl(get(), get()) }
 
+    single<ProductRepository> {
+        ProductRepositoryImpl(
+            get(),
+            get<AppDatabase>().productDao() //we need productDao from appDatabase
+        )
+    }
+
+
+//ViewModels
     viewModel { SignUpViewModel(get()) }
     viewModel { SignInViewModel(get()) }
+    viewModel { (isInternetConnected: Boolean) -> MainViewModel(get(), isInternetConnected) }
 
 }
