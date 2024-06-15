e411361fb185e912935cf760729efb060a677da2
mona-baharlou <mona.baharlou755@gmail.com>
2024-06-15 15:49:54 +0330

    MainScreen.kt updated--> AdvertisementSection added

--- app/src/main/java/com/bahrlou/shoppingapp/ui/features/main/MainScreen.kt
+++ app/src/main/java/com/bahrlou/shoppingapp/ui/features/main/MainScreen.kt
@@ -5,9 +5,10 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -25,7 +26,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -70,7 +71,7 @@ fun MainScreen() {
         CategorySection()
         ProductByCategory()//subject
         ProductByCategory()
-        AdvPic()
+        AdvertisementSection()
         ProductByCategory()
         ProductByCategory()
     }
@@ -221,8 +222,18 @@ fun ProductItem() {
 //**************************** Advertisement Image **********************************/
 
 @Composable
-fun AdvPic() {
-
+fun AdvertisementSection() {
+    Image(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(260.dp)
+            .padding(top = 32.dp, start = 16.dp, end = 16.dp)
+            .clip(shape = Shapes.medium)
+            .clickable { },
+        painter = painterResource(id = R.drawable.img_intro),
+        contentDescription = null,
+        contentScale = ContentScale.Crop
+    )
 }
 
 @Composable
