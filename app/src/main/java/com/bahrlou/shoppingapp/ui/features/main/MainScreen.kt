6f5d607a5ab236550a29a88733950669fad932a5
mona-baharlou <mona.baharlou755@gmail.com>
2024-06-15 14:46:21 +0330

    MainScreen.kt updated--> related sections are added

--- app/src/main/java/com/bahrlou/shoppingapp/ui/features/main/MainScreen.kt
+++ app/src/main/java/com/bahrlou/shoppingapp/ui/features/main/MainScreen.kt
@@ -1,12 +1,20 @@
 package com.bahrlou.shoppingapp.ui.features.main
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.bahrlou.shoppingapp.ui.theme.BackgroundMain
 import com.bahrlou.shoppingapp.ui.theme.ShoppingAppTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 
 @Preview(showBackground = true)
@@ -25,5 +33,58 @@ fun MainScreenPreview() {
 @Composable
 fun MainScreen() {
 
+    ChangeStatusBarColor()
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .padding(bottom = 16.dp)
+
+    ) {
+        TopToolbar()
+        CategorySection()
+        ProductByCategory()//subject
+        ProductByCategory()
+        AdvPic()
+        ProductByCategory()
+        ProductByCategory()
+    }
 
 }
+
+//**************************** TOOLBAR **********************************/
+@Composable
+fun TopToolbar() {
+
+}
+
+//**************************** CATEGORY **********************************/
+
+@Composable
+fun CategorySection() {
+
+}
+
+//**************************** Products **********************************/
+
+@Composable
+fun ProductByCategory() {
+
+}
+//**************************** Advertisement Image **********************************/
+
+@Composable
+fun AdvPic() {
+
+}
+
+@Composable
+fun ChangeStatusBarColor() {
+    val uiController = rememberSystemUiController()
+
+    SideEffect {
+        uiController.setStatusBarColor(Color.White)
+    }
+}
+
