18a9ac827de61f1c1243a10fc848ead7e5df2c30
mona-baharlou <mona.baharlou755@gmail.com>
2024-06-16 11:38:47 +0330

    CategoryScreen.kt updated

--- app/src/main/java/com/bahrlou/shoppingapp/ui/features/category/CategoryScreen.kt
+++ app/src/main/java/com/bahrlou/shoppingapp/ui/features/category/CategoryScreen.kt
@@ -1,14 +1,35 @@
 package com.bahrlou.shoppingapp.ui.features.category
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Card
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import com.bahrlou.shoppingapp.model.data.Product
+import com.bahrlou.shoppingapp.ui.theme.Blue
+import com.bahrlou.shoppingapp.ui.theme.Shapes
 import com.bahrlou.shoppingapp.util.MyScreens
 import dev.burnoo.cokoin.navigation.getNavController
 import dev.burnoo.cokoin.viewmodel.getViewModel
@@ -30,6 +51,7 @@ fun CategoryScreen(categoryName: String) {
 
     Column(
         modifier = Modifier.fillMaxSize()
+
     ) {
 
         CategoryToolbar(categoryName)
@@ -45,16 +67,95 @@ fun CategoryScreen(categoryName: String) {
 @Composable
 fun Products(productList: List<Product>, onProductClicked: (String) -> Unit) {
 
-
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        items(productList.size) {
+            ProductItem(data = productList[it], onProductClicked)
+        }
+    }
 }
 
 @Composable
 fun ProductItem(data: Product, onProductClicked: (String) -> Unit) {
 
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 16.dp, end = 16.dp, top = 16.dp)
+            .clickable { onProductClicked.invoke(data.productId) },
+        elevation = 4.dp,
+        shape = Shapes.large
+    ) {
 
+        Column() {
+            AsyncImage(
+                model = data.imgUrl,
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+            )
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Column(modifier = Modifier.padding(10.dp)) {
+
+                    Text(
+                        text = data.name,
+                        style = TextStyle(fontSize = 15.sp, fontWeight = FontWeight.Medium)
+                    )
+
+                    Text(
+                        modifier = Modifier.padding(top = 4.dp),
+                        text = "$" + data.price,
+                        style = TextStyle(fontSize = 14.sp)
+                    )
+
+                }
+
+
+
+                Surface(
+                    modifier = Modifier
+                        .padding(start = 8.dp, end = 8.dp, bottom = 8.dp)
+                        .align(Alignment.Bottom)
+                        .clip(Shapes.large),
+                    color = Blue
+                ) {
+                    Text(
+                        modifier = Modifier.padding(4.dp),
+                        text = data.soldItem,
+                        style = TextStyle(
+                            fontSize = 13.sp,
+                            fontWeight = FontWeight.Medium,
+                            color = Color.White
+                        )
+                    )
+
+                }
+
+            }
+
+
+        }
+
+    }
 }
 
 @Composable
 fun CategoryToolbar(categoryName: String) {
-
+    TopAppBar(
+        elevation = 0.dp,
+        backgroundColor = Color.White,
+        modifier = Modifier.fillMaxWidth(),
+        title = {
+            Text(
+                text = categoryName,
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
+        }
+    )
 }
