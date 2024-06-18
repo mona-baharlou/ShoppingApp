e8092024771100a75cb29ffd60ca8a97410489d0
mona-baharlou <mona.baharlou755@gmail.com>
2024-06-18 23:50:56 +0330

    ShowInfoSection added to ProfileScreen.kt

--- app/src/main/java/com/bahrlou/shoppingapp/ui/features/profile/ProfileScreen.kt
+++ app/src/main/java/com/bahrlou/shoppingapp/ui/features/profile/ProfileScreen.kt
@@ -1,5 +1,6 @@
 package com.bahrlou.shoppingapp.ui.features.profile
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
@@ -11,6 +12,7 @@ import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Surface
@@ -24,15 +26,19 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.LottieConstants
 import com.airbnb.lottie.compose.rememberLottieComposition
 import com.bahrlou.shoppingapp.R
 import com.bahrlou.shoppingapp.ui.theme.BackgroundMain
+import com.bahrlou.shoppingapp.ui.theme.Blue
 import com.bahrlou.shoppingapp.ui.theme.ShoppingAppTheme
 import dev.burnoo.cokoin.navigation.getNavController
 import dev.burnoo.cokoin.viewmodel.getViewModel
@@ -104,8 +110,44 @@ fun ProfileToolbar(OnBackClicked: () -> Unit) {
 }
 
 @Composable
-fun ShowInfoSection() {
+fun ShowInfoSection(
+    subject: String,
+    text: String,
+    OnLocationClicked: (() -> Unit)?
+) {
 
+    Column(
+        modifier = Modifier
+            .padding(start = 16.dp, top = 16.dp, end = 16.dp)
+            .clickable { OnLocationClicked?.invoke() },
+        horizontalAlignment = Alignment.Start
+    ) {
+
+        Text(
+            text = subject,
+            style = TextStyle(
+                fontSize = 18.sp,
+                color = Blue,
+                fontWeight = FontWeight.Bold
+            )
+        )
+
+
+        Text(
+            text = text,
+            modifier = Modifier.padding(top = 2.dp),
+            style = TextStyle(
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Medium
+            )
+        )
+
+        Divider(
+            color = Blue, thickness = 0.5.dp,
+            modifier = Modifier.padding(top = 16.dp)
+        )
+
+    }
 
 }
 
