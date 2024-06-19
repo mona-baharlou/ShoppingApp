60adbd126a744b43729db7412262156141570743
mona-baharlou <mona.baharlou755@gmail.com>
2024-06-19 14:10:12 +0330

    ProfileScreen.kt updated -> AddUserLocationDataDialog added

--- app/src/main/java/com/bahrlou/shoppingapp/ui/features/profile/ProfileScreen.kt
+++ app/src/main/java/com/bahrlou/shoppingapp/ui/features/profile/ProfileScreen.kt
@@ -2,29 +2,39 @@ package com.bahrlou.shoppingapp.ui.features.profile
 
 import android.widget.Toast
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
+import androidx.compose.material.Card
+import androidx.compose.material.Checkbox
 import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -35,13 +45,16 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.LottieConstants
 import com.airbnb.lottie.compose.rememberLottieComposition
 import com.bahrlou.shoppingapp.R
+import com.bahrlou.shoppingapp.ui.features.product.MainTextField
 import com.bahrlou.shoppingapp.ui.theme.BackgroundMain
 import com.bahrlou.shoppingapp.ui.theme.Blue
+import com.bahrlou.shoppingapp.ui.theme.Shapes
 import com.bahrlou.shoppingapp.ui.theme.ShoppingAppTheme
 import com.bahrlou.shoppingapp.util.MyScreens
 import dev.burnoo.cokoin.navigation.getNavController
@@ -204,5 +217,114 @@ fun MainAnimation() {
         iterations = LottieConstants.IterateForever
     )
 }
+
+@Composable
+fun AddUserLocationDialog(
+    showSaveLocation: Boolean,
+    onDismiss: () -> Unit,
+    onSubmitClicked: (String, String, Boolean) -> Unit
+) {
+
+    val context = LocalContext.current
+    val checkedState = remember { mutableStateOf(true) }
+    val userAddress = remember { mutableStateOf("") }
+    val userPostalCode = remember { mutableStateOf("") }
+    val fraction = if (showSaveLocation) 0.695f else 0.625f
+
+    Dialog(onDismissRequest = onDismiss) {
+
+        Card(
+            modifier = Modifier.fillMaxHeight(fraction),
+            elevation = 8.dp,
+            shape = Shapes.medium
+        ) {
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceAround
+            ) {
+
+                Text(
+                    text = "Add Location Data",
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 18.sp,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                )
+                Spacer(modifier = Modifier.height(6.dp))
+
+                MainTextField(userAddress.value, "your address...") {
+                    userAddress.value = it
+                }
+
+                MainTextField(userPostalCode.value, "your postal code...") {
+                    userPostalCode.value = it
+                }
+
+                if (showSaveLocation) {
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 12.dp, start = 4.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+
+                        Checkbox(
+                            checked = checkedState.value,
+                            onCheckedChange = { checkedState.value = it },
+                        )
+
+                        Text(text = "Save To Profile")
+
+                    }
+
+                }
+
+
+                // Buttons
+                Row(
+                    horizontalArrangement = Arrangement.End,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+
+                    TextButton(onClick = onDismiss) {
+                        Text(text = "Cancel")
+                    }
+                    Spacer(modifier = Modifier.width(4.dp))
+                    TextButton(onClick = {
+
+                        if (
+                            (userAddress.value.isNotEmpty() || userAddress.value.isNotBlank()) &&
+                            (userPostalCode.value.isNotEmpty() || userPostalCode.value.isNotBlank())
+                        ) {
+                            onSubmitClicked(
+                                userAddress.value,
+                                userPostalCode.value,
+                                checkedState.value
+                            )
+                            onDismiss.invoke()
+                        } else {
+                            Toast.makeText(context, "please write first...", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+
+
+                    }) {
+                        Text(text = "Ok")
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
 
 
